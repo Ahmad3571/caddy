3c1a208564b5d7dce75cc08905dc5bc2a0674ff1
Ahmad <https4uk@gmail.com>
2024-10-27 22:54:12 +0500

    added

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#element", {
+  strings: ["So Beautiful ", "So Delicious", "So Cute","So Intelligent","So Gorgeous"],
+  typeSpeed: 50,
+  backspeed: 50,
+  backdelay: 500,
+  loop:true 
+});
